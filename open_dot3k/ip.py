d07d48ce53c6f0688a2bd46358ac056a991ab4dd
arceusVen1 <jeremy.venin@telecom-sudparis.eu>
2016-11-22 18:24:22 +0100

    ip class working

---
+++ open_dot3k/ip.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+
+import netifaces as ni
+
+
+class IP():
+    """represent the ip adress"""
+
+    def __init__(self):
+        self.address = ""
+
+    def get_address(self):
+        try:
+            self.address = ni.ifaddresses('eth0')[ni.AF_INET][0]['addr']
+        except:
+            self.address = "Not connected"
