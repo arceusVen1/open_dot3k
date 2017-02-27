6e964ed73ef7c48c05a45fada3f8f259ddf01a3c
arceusVen1 <jeremy.venin@telecom-sudparis.eu>
2017-02-27 19:09:03 +0100

    set_size specific

--- open_dot3k/ledbar.py
+++ open_dot3k/ledbar.py
@@ -6,12 +6,13 @@ import dothat.backlight as backlight
 class LedBar():
 
     def __init__(self):
-        self.maxtemp = 50.0  # warning: maxtemp must be a float !
-        return
+        pass
 
-    def set_size(self, temp):
-        for i in range(int(temp)):
-            backlight.set_graph(i / self.maxtemp)
+    def set_size(self, value, max):
+        if max is None:
+            max = 50
+        for i in range(int(value)):
+            backlight.set_graph(i / max)
 
     def led_zero(self):
         backlight.set_graph(0)
